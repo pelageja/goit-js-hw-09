60d86963491c388b420cffaadc8b65c89c6e4db6
pelageja <29102014n@gmail.com>
2022-07-14 16:42:29 +0300

    task2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+
+function  getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function updateBodyBGcolor(color) {
+  document.body.style.backgroundColor = color;
+}
+class ColorSwitcher {
+  constructor(updateBodyBGcolor) {
+    this.intervalID = null;
+    this.isActive = false;
+    this.updateBodyBGcolor = updateBodyBGcolor;
+    stopBtn.disabled = true;
+  }
+
+  startChangeBGcolor() {
+    if (this.isActive) {
+      return;
+    }
+
+   startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    this.isActive = true;
+    this.intervalID = setInterval(() => updateBodyBGcolor(getRandomHexColor()), 1000);
+  }
+
+  stopChangeBGcolor() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(this.intervalID);
+    this.isActive = false;
+  }
+}
+
+const colorSwitcher = new ColorSwitcher();
+
+startBtn.addEventListener('click', () => colorSwitcher.startChangeBGcolor());
+stopBtn.addEventListener('click', () => colorSwitcher.stopChangeBGcolor());
 
